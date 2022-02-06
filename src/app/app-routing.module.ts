8796676cc956a9178e8c84ff343b5ab5a80b0c3e
shivasai <muthineanishivasai@gmail.com>
2022-02-06 15:32:29 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'subscribers', loadChildren: () => import('./modules/subscribers/subscribers.module').then(m => m.SubscribersModule)
+  },
+  {
+    path: 'content', loadChildren: () => import('./modules/content/content.module').then(m => m.ContentModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/subscribers/subscribers.module.ts
+++ src/app/modules/subscribers/subscribers.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RootComponent } from './components/root/root.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/app/shared-modules/shared/shared.module';
 
-
+const routes: Routes = [
+  {
+    path: '', component: RootComponent
+  }
+]
 
 @NgModule({
   declarations: [
     RootComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)
+
   ]
 })
 export class SubscribersModule { }
